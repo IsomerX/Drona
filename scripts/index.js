6844a3c1f79316f6c290376e48b2caa5d6176c49
IsomerX <dhruvbakshi12343@gmail.com>
2019-05-04 19:23:53 +0530

    final

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,41 @@
 $(document).ready(function () {
-	var controller = new ScrollMagic.Controller();
+	
+	
 
-	var ourScene = new ScrollMagic.Scene({
-			triggerElement: '#hero'
-		})
-		.setClassToggle('#hero', 'fade-in')
-		.addTo(controller)
-
+	var graphs = document.getElementsByClassName('graph')
+	graphs[0].addEventListener("click",function(){
+		var y =document.getElementsByClassName('focus')
+		y[0].classList.remove("focus")
+		graphs[0].classList.add("focus")
+	})
+	graphs[1].addEventListener("click",function(){
+		var y =document.getElementsByClassName('focus')
+		y[0].classList.remove("focus")
+		graphs[1].classList.add("focus")
+	})
+	graphs[2].addEventListener("click",function(){
+		var y =document.getElementsByClassName('focus')
+		y[0].classList.remove("focus")
+		graphs[2].classList.add("focus")
+	})
+	graphs[3].addEventListener("click",function(){
+		var y =document.getElementsByClassName('focus')
+		y[0].classList.remove("focus")
+		graphs[3].classList.add("focus")
+	})
+	graphs[4].addEventListener("click",function(){
+		var y =document.getElementsByClassName('focus')
+		y[0].classList.remove("focus")
+		graphs[4].classList.add("focus")
+	})
+	graphs[5].addEventListener("click",function(){
+		var y =document.getElementsByClassName('focus')
+		y[0].classList.remove("focus")
+		graphs[5].classList.add("focus")
+	})
+	graphs[6].addEventListener("click",function(){
+		var y =document.getElementsByClassName('focus')
+		y[0].classList.remove("focus")
+		graphs[6].classList.add("focus")
+	})
 })
